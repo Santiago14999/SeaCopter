3753e779b6523721641dc4983ad842b621365da1
rogozhkin911 <36700226+rogozhkin911@users.noreply.github.com>
2020-07-13 00:40:47 +0700

    Procedural Waves Added

---
+++ Assets/Scripts/Waves/Editor/WaterWavesControllerEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(WaterWavesController))]
+public class WaterWavesControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WaterWavesController wavesController = (WaterWavesController)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (wavesController.showPreview)
+                wavesController.GenerateMeshPreview();
+        }
+    }
+}
---
+++ Assets/Scripts/Waves/Noise.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class Noise
+{
+    public static void GenerateNoiseMap(float[,] arrayToFill, int mapWidth, int mapHeight, float scale, Vector2 offset)
+	{
+		if (scale <= 0)
+			scale = 0.0001f;
+
+		float halfWidth = mapWidth / 2f;
+		float halfHeight = mapHeight / 2f;
+
+		for (int y = 0; y < mapHeight; y++)
+		{
+			for (int x = 0; x < mapWidth; x++)
+			{
+				float sampleX = (x - halfWidth) / scale + offset.x;
+				float sampleY = (y - halfHeight) / scale + offset.y;
+
+				float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+
+				arrayToFill[x, y] = perlinValue;
+			}
+		}
+	}
+}
