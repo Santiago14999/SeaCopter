4610108c3ef75a9298c801deb4a93394a1968fe9
rogozhkin911 <36700226+rogozhkin911@users.noreply.github.com>
2020-07-16 14:41:55 +0700

    Removed unused assets. Compressed models.

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -2,23 +2,22 @@
 
 public class InputHandler : MonoBehaviour
 {
-    [SerializeField] private VariableJoystick _joystick;
+    [SerializeField] private VariableJoystick _movementJoystick;
+    [SerializeField] private VariableJoystick _heightJoystick;
 
     public Vector3 GetMoveInput()
     {
-        if (!_joystick)
-        {
-            float xInput = Input.GetAxis("Horizontal");
-            float zInput = Input.GetAxis("Vertical");
-            float yInput = Input.GetAxis("Elevation");
+        //float xInput = Input.GetAxis("Horizontal");
+        //float zInput = Input.GetAxis("Vertical");
+        //float yInput = Input.GetAxis("Elevation");
 
-            Vector3 moveVector = new Vector3(xInput, yInput, zInput);
+        //Vector3 moveVector = new Vector3(xInput, yInput, zInput);
 
-            return moveVector.normalized;
-        }
+        //return moveVector.normalized;
 
-        Vector2 joystickInput = _joystick.Direction;
+        Vector2 joystickInput = _movementJoystick.Direction;
+        Vector2 heightInput = _heightJoystick.Direction;
 
-        return new Vector3(joystickInput.x, 0, joystickInput.y);
+        return new Vector3(joystickInput.x, heightInput.y, joystickInput.y);
     }
 }
